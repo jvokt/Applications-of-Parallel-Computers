811e61674ecc4c8db4b33a43ef3ae8b7af1b1f5b
Phillip Tischler <pmt43@cornell.edu>
2014-02-13 20:57:52 -0500

    Changed from recursive blocked to just blocked

--- hw1/dgemm_mine.c
+++ hw1/dgemm_mine.c
@@ -6,74 +6,11 @@
 
 const char* dgemm_desc = "My awesome dgemm.";
 
-#ifndef BLOCK_SIZE
-#define BLOCK_SIZE ((int) 4)
-#endif
-
-/*******************************************************************************
- * Program constants
- */
-
-#ifndef BYTE_ALIGNMENT
-#define BYTE_ALIGNMENT 16
-#endif
-
-#ifndef NUM_MAT
-#define NUM_MAT 3
-#endif
-
-/*******************************************************************************
- * Cache size and utilization definitions
- */
-
-#ifndef L1_CACHE_SIZE
-#define L1_CACHE_SIZE ((int) 32768) // 32KB
-#endif
-
-#ifndef L1_CACHE_UTILIZATION
-#define L1_CACHE_UTILIZATION ((double) 0.5)
-#endif
-
-#ifndef L2_CACHE_SIZE
-#define L2_CACHE_SIZE ((int) 262144) // 256KB
-#endif
-
-#ifndef L2_CACHE_UTILIZATION
-#define L2_CACHE_UTILIZATION ((double) 0.5)
-#endif
-
-#ifndef L3_CACHE_SIZE
-#define L3_CACHE_SIZE ((int) 4194304) // 4MB
-#endif
-
-#ifndef L3_CACHE_UTILIZATION
-#define L3_CACHE_UTILIZATION ((double) 0.75)
-#endif
-
-
-/*******************************************************************************
- * Kernel and block size calculations
- */
-
-// Calculate the size of kernels
-// block_size_bytes = (int)(floor(sqrt(CACHE_SIZE - 3*16) * CACHE_UTIL / (3 * sizeof(double))))
-// block_size_bytes = block_size_bytes + (16 - (block_size_bytes % 16))
-#define KERNEL_SIZE_UNALIGNED(CACHE_SIZE, CACHE_UTIL) ((int)(floor(sqrt(CACHE_SIZE - NUM_MAT * BYTE_ALIGNMENT) * CACHE_UTIL / (NUM_MAT * sizeof(double)))))
-#define KERNEL_SIZE_ALIGNED(CACHE_SIZE, CACHE_UTIL) (KERNEL_SIZE_UNALIGNED(CACHE_SIZE, CACHE_UTIL) + BYTE_ALIGNMENT - (KERNEL_SIZE_UNALIGNED(CACHE_SIZE, CACHE_UTIL) % BYTE_ALIGNMENT))
-
 // Define the kernels as the closest value rounded to be the smallest multiple
 // of the lower block such that the block is larger
 
 #ifndef L1_KERNEL_P
 #define L1_KERNEL_P 32 // (KERNEL_SIZE_ALIGNED(L1_CACHE_SIZE, L1_CACHE_UTILIZATION))
-#endif
-
-#ifndef L2_BLOCK_SIZE
-#define L2_BLOCK_SIZE 96 // (KERNEL_SIZE_ALIGNED(L2_CACHE_SIZE, L2_CACHE_UTILIZATION))
-#endif
-
-#ifndef L3_BLOCK_SIZE
-#define L3_BLOCK_SIZE 384 // (KERNEL_SIZE_ALIGNED(L3_CACHE_SIZE, L3_CACHE_UTILIZATION))
 #endif
 
 #if L1_KERNEL_P != KERNEL_P
@@ -108,163 +45,55 @@ double kernel_A[2 * L1_KERNEL_P] __attribute__ ((aligned (BYTE_ALIGNMENT)));
 double kernel_B[2 * L1_KERNEL_P] __attribute__ ((aligned (BYTE_ALIGNMENT)));
 double kernel_C[2 * 2] __attribute__ ((aligned (BYTE_ALIGNMENT)));
 
-/*
-  A is M-by-K
-  B is K-by-N
-  C is M-by-N
-
-  lda is the leading dimension of the matrix (the M of square_dgemm).
-*/
-void basic_dgemm(const int lda, const int M, const int N, const int K,
-                 const double *A, const double *B, double *C)
-{
-	int i, j, k;
-
-	if (M != KERNEL_M || N != KERNEL_N || K != KERNEL_P)
-	{
-		// Do basic method
-		for (i = 0; i < M; ++i) {
-			for (j = 0; j < N; ++j) {
-				double cij = C[j*lda+i];
-				for (k = 0; k < K; ++k) {
-					cij += A[k*lda+i] * B[j*lda+k];
-				}
-				C[j*lda+i] = cij;
-			}
-		}
-	}
-	else
-	{
-		// Copy optimization to kernel memory
-		to_kdgemm_A(lda, A, kernel_A);
-		to_kdgemm_B(lda, B, kernel_B);
-		to_kdgemm_C(lda, C, kernel_C);
-
-		// Execute kernel
-		kdgemm(kernel_A, kernel_B, kernel_C);
-
-		// Copy back from kernel memory
-		from_kdgemm_C(lda, kernel_C, C);
-	}
-}
-
-void do_block(const int lda,
-              const double *A, const double *B, double *C,
-              const int i, const int j, const int k)
-{
-    const int M = (i+BLOCK_SIZE > lda? lda-i : BLOCK_SIZE);
-    const int N = (j+BLOCK_SIZE > lda? lda-j : BLOCK_SIZE);
-    const int K = (k+BLOCK_SIZE > lda? lda-k : BLOCK_SIZE);
-    basic_dgemm(lda, M, N, K,
-                A + i + k*lda, B + k + j*lda, C + i + j*lda);
-}
-
 void square_dgemm(const int M, const double *A, const double *B, double *C)
 {
-	// This function works by performing recursively blocked matrix multiply.
-	// The highest level works so that the block size causes the 3 blocks of the
-	// matrix operation to work entirely in L3 cache. The second level works so
-	// that its block size causes all 3 blocks to fall in L2 cache. The final
-	// level uses the kernel which operates on PxP matrices in a 2xP blocked
-	// fashion. The first step is to calculate the various block sizes and then
-	// they will be executed. The various block sizes were calculated so that
-	// boundaries are aligned in memory, which means once copied into aligned
-	// memory the alignment won't be an issue
-
 	// Get the number of blocks l3 blocks in M
-	const int num_l3_blocks = CALC_NUM_BLOCKS(M, L3_BLOCK_SIZE);
+	const int num_blocks = CALC_NUM_BLOCKS(M, L1_KERNEL_P);
 
-	// Perform blocked multiplication with L3_BLOCK_SIZE sized blocks
-	for(int iter_main_col_block = 0; iter_main_col_block < num_l3_blocks; ++iter_main_col_block)
+	// Perform blocked multiplication with L1_KERNEL_P sized blocks
+	for(int iter_col_block = 0; iter_col_block < num_blocks; ++iter_col_block)
 	{
-		const int cur_main_col_pos = iter_main_col_block * L3_BLOCK_SIZE;
-		const int cur_main_col_width = CALC_CUR_BLOCK_WIDTH(cur_main_col_pos, L3_BLOCK_SIZE, M);
-		const int num_l2_blocks_col = CALC_NUM_BLOCKS(cur_main_col_width, L2_BLOCK_SIZE);
+		const int cur_col = iter_col_block * L1_KERNEL_P;
+		const int cur_col_num = CALC_CUR_BLOCK_WIDTH(cur_col, L1_KERNEL_P, M);
+		const int num_blocks_kernel_col = CALC_NUM_BLOCKS(cur_col_num, 2);
 
-		for(int iter_l3_row_block = 0; iter_l3_row_block < num_l3_blocks; ++iter_l3_row_block)
+		for(int iter_row_block = 0; iter_row_block < num_blocks; ++iter_row_block)
 		{
-			const int cur_main_row_pos = iter_l3_row_block * L3_BLOCK_SIZE;
-			const int cur_main_row_width = CALC_CUR_BLOCK_WIDTH(cur_main_row_pos, L3_BLOCK_SIZE, M);
-			const int num_l2_blocks_row = CALC_NUM_BLOCKS(cur_main_row_width, L2_BLOCK_SIZE);
+			const int cur_row = iter_row_block * L1_KERNEL_P;
+			const int cur_row_num = CALC_CUR_BLOCK_WIDTH(iter_row_block, L1_KERNEL_P, M);
+			const int num_blocks_kernel_row = CALC_NUM_BLOCKS(cur_row_num, 2);
 
-			for(int iter_main_accum_block = 0; iter_main_accum_block < num_l3_blocks; ++iter_main_accum_block)
+			for(int iter_accum_block = 0; iter_accum_block < num_blocks; ++iter_accum_block)
 			{
-				const int cur_main_accum_pos = iter_main_accum_block * L3_BLOCK_SIZE;
-				const int cur_main_accum_width = CALC_CUR_BLOCK_WIDTH(cur_main_accum_pos, L3_BLOCK_SIZE, M);
-				const int num_l2_blocks_accum = CALC_NUM_BLOCKS(cur_main_accum_width, L2_BLOCK_SIZE);
+				const int cur_accum = iter_accum_block * L1_KERNEL_P;
+				const int cur_accum_num = CALC_CUR_BLOCK_WIDTH(cur_accum, L1_KERNEL_P, M);
 
-				// Perform blocked multiplication with L2_BLOCK_SIZE sized blocks
-				for(int iter_l3_col_block = 0; iter_l3_col_block < num_l2_blocks_col; ++iter_l3_col_block)
+				// Now have sizes of L1_KERNEL_P^2 or less. Use kernel which calculates blocks
+				// of size [2x2] = [2xL1_KERNEL_P] [L1_KERNEL_Px2]
+
+				// Perform kernel operations for each [2x2] block within the L1_KERNEL_P sized block
+				for(int iter_kernel_col_block = 0; iter_kernel_col_block < num_blocks_kernel_col; ++iter_kernel_col_block)
 				{
-					const int cur_l3_col_pos = iter_l3_col_block * L2_BLOCK_SIZE;
-					const int cur_l3_col_width = CALC_CUR_BLOCK_WIDTH(cur_l3_col_pos, L2_BLOCK_SIZE, cur_main_col_width);
-					const int num_l1_blocks_col = CALC_NUM_BLOCKS(cur_l3_col_width, L1_KERNEL_P);
+					const int cur_kernel_col = 2 * iter_kernel_col_block;
+					const int cur_kernel_col_num = CALC_CUR_BLOCK_WIDTH(cur_kernel_col, 2, cur_col_num);
 
-					for(int iter_l3_row_block = 0; iter_l3_row_block < num_l2_blocks_row; ++iter_l3_row_block)
+					for(int iter_kernel_row_block = 0; iter_kernel_row_block < num_blocks_kernel_row; ++iter_kernel_row_block)
 					{
-						const int cur_l3_row_pos = iter_l3_row_block * L2_BLOCK_SIZE;
-						const int cur_l3_row_width = CALC_CUR_BLOCK_WIDTH(cur_l3_row_pos, L2_BLOCK_SIZE, cur_main_row_width);
-						const int num_l1_blocks_row = CALC_NUM_BLOCKS(cur_l3_row_width, L1_KERNEL_P);
+						const int cur_kernel_row = 2 * iter_kernel_row_block;
+						const int cur_kernel_row_num = CALC_CUR_BLOCK_WIDTH(cur_kernel_row, 2, cur_row_num);
 
-						for(int iter_l3_accum_block = 0; iter_l3_accum_block < num_l2_blocks_accum; ++iter_l3_accum_block)
-						{
-							const int cur_l3_accum_pos = iter_l3_accum_block * L2_BLOCK_SIZE;
-							const int cur_l3_accum_width = CALC_CUR_BLOCK_WIDTH(cur_l3_accum_pos, L2_BLOCK_SIZE, cur_main_accum_width);
-							const int num_l1_blocks_accum = CALC_NUM_BLOCKS(cur_l3_accum_width, L1_KERNEL_P);
+						// Copy data into kernel memory buffers (copy optimization 2 & memory layout)
+						// Because of L3 memory size and the zero-ing operation, this will fit in the
+						// kernel space and have 0s where invalid
+						to_kdgemm_A_sized(M, A + M * (cur_accum) + (cur_row + cur_kernel_row), kernel_A, cur_kernel_row_num, cur_col_num);
+						to_kdgemm_B_sized(M, B + M * (cur_col + cur_kernel_col) + (cur_accum), kernel_B, cur_row_num, cur_kernel_col_num);
+						to_kdgemm_C_sized(M, C + M * (cur_col + cur_kernel_col) + (cur_row + cur_kernel_row), kernel_C, cur_kernel_row_num, cur_kernel_col_num);
 
-							// Perform blocked multiplication with L1_KERNEL_P sized blocks
-							for(int iter_l2_col_block = 0; iter_l2_col_block < num_l1_blocks_col; ++iter_l2_col_block)
-							{
-								const int cur_l2_col_pos = iter_l2_col_block * L1_KERNEL_P;
-								const int cur_l2_col_width = CALC_CUR_BLOCK_WIDTH(cur_l2_col_pos, L1_KERNEL_P, cur_l3_col_width);
-								const int num_kernel_blocks_col = CALC_NUM_BLOCKS(cur_l2_col_width, 2);
+						// Perform kernel operations
+						kdgemm(kernel_A, kernel_B, kernel_C);
 
-								for(int iter_l2_row_block = 0; iter_l2_row_block < num_l1_blocks_row; ++iter_l2_row_block)
-								{
-									const int cur_l2_row_pos = iter_l2_row_block * L1_KERNEL_P;
-									const int cur_l2_row_width = CALC_CUR_BLOCK_WIDTH(cur_l2_row_pos, L1_KERNEL_P, cur_l3_row_width);
-									const int num_kernel_blocks_row = CALC_NUM_BLOCKS(cur_l2_row_width, 2);
-
-									for(int iter_l2_accum_block = 0; iter_l2_accum_block < num_l1_blocks_accum; ++iter_l2_accum_block)
-									{
-										const int cur_l2_accum_pos = iter_l2_accum_block * L1_KERNEL_P;
-										const int cur_l2_accum_width = CALC_CUR_BLOCK_WIDTH(cur_l2_accum_pos, L1_KERNEL_P, cur_l3_accum_width);
-
-										// Now have sizes of L1_KERNEL_P^2 or less. Use kernel which calculates blocks
-										// of size [2x2] = [L1_KERNEL_Px2] [2xL1_KERNEL_P]
-
-										// Perform kernel operations for each [2x2] block within the L1_KERNEL_P sized block
-										for(int iter_kernel_col_block = 0; iter_kernel_col_block < num_kernel_blocks_col; ++iter_kernel_col_block)
-										{
-											const int cur_kernel_col_pos = 2 * iter_kernel_col_block;
-											const int cur_kernel_col_width = CALC_CUR_BLOCK_WIDTH(cur_kernel_col_pos, 2, cur_l2_col_width);
-
-											for(int iter_kernel_row_block = 0; iter_kernel_row_block < num_kernel_blocks_row; ++iter_kernel_row_block)
-											{
-												const int cur_kernel_row_pos = 2 * iter_kernel_row_block;
-												const int cur_kernel_row_width = CALC_CUR_BLOCK_WIDTH(cur_kernel_row_pos, 2, cur_l2_row_width);
-
-												// Now have complete offset into l3 memory buffer from which to copy data
-												// into the kernel memory buffers
-
-												// Copy data into kernel memory buffers (copy optimization 2 & memory layout)
-												// Because of L3 memory size and the zero-ing operation, this will fit in the
-												// kernel space and have 0s where invalid
-												to_kdgemm_A_sized(M, A + M * (cur_main_accum_pos + cur_l3_accum_pos + cur_l2_accum_pos) + cur_main_row_pos + cur_l3_row_pos + cur_l2_row_pos + cur_kernel_row_pos, kernel_A, cur_kernel_row_width, cur_l2_col_width);
-												to_kdgemm_B_sized(M, B + M * (cur_main_col_pos + cur_l3_col_pos + cur_l2_col_pos + cur_kernel_col_pos) + cur_main_accum_pos + cur_l3_accum_pos + cur_l2_accum_pos, kernel_B, cur_l2_row_width, cur_kernel_col_width);
-												to_kdgemm_C_sized(M, C + M * (cur_main_col_pos + cur_l3_col_pos + cur_l2_col_pos + cur_kernel_col_pos) + cur_main_row_pos + cur_l3_row_pos + cur_l2_row_pos + cur_kernel_row_pos, kernel_C, cur_kernel_row_width, cur_kernel_col_width);
-
-												// Perform kernel operations
-												kdgemm(kernel_A, kernel_B, kernel_C);
-
-												// Copy results out of kernel memory buffers
-												from_kdgemm_C_sized(M, kernel_C, C + M * (cur_main_col_pos + cur_l3_col_pos + cur_l2_col_pos + cur_kernel_col_pos) + cur_main_row_pos + cur_l3_row_pos + cur_l2_row_pos + cur_kernel_row_pos, cur_kernel_row_width, cur_kernel_col_width);
-											}
-										}
-									}
-								}
-							}
-						}
+						// Copy results out of kernel memory buffers
+						from_kdgemm_C_sized(M, kernel_C, C + M * (cur_col + cur_kernel_col) + (cur_row + cur_kernel_row), cur_kernel_row_num, cur_kernel_col_num);
 					}
 				}
 			}
