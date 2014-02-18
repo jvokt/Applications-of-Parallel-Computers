5f79a4b94bb799e527608eba6c1591947fba3da8
Phillip Tischler <pmt43@cornell.edu>
2014-02-18 00:35:42 -0500

    Selected values for grid

--- hw1/tuner-matmul_eval.py
+++ hw1/tuner-matmul_eval.py
@@ -9,21 +9,21 @@ import numpy
 isDistributed = False
 
 # Define the parameter values
-allVals = range(16,176+1,16)
+aVals = [192, 240, 256, 304, 336];
+cVals = [8, 16, 32, 40, 48, 64];
 
 # Execute csub script to run make across parameter grid
 print 'Launching the Single Tuners'
 totalTuners = len(allVals)
 curTuner = 1
-for curVal in allVals:
-    curA = curVal
-    curC = curVal
-    print '\tLaunching Tuner ' + str(curTuner) + ' of ' + str(totalTuners)
-    baseCommand = 'python tuner-matmul_eval-single.py ' + str(curA) + ' ' + str(curC) 
-    if isDistributed:
-        os.system('csub ' + baseCommand)
-    else:
-        os.system(baseCommand)
-    curTuner += 1
+for curA in aVals:
+    for curC in cVals:
+        print '\tLaunching Tuner ' + str(curTuner) + ' of ' + str(totalTuners)
+        baseCommand = 'python tuner-matmul_eval-single.py ' + str(curA) + ' ' + str(curC) 
+        if isDistributed:
+            os.system('csub ' + baseCommand)
+        else:
+            os.system(baseCommand)
+        curTuner += 1
                 
 print 'Completed'
