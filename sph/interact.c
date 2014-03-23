4991ffb134b4630046741557b82e3fe887f6e101
Phillip Tischler <pmt43@cornell.edu>
2014-03-22 21:26:37 -0400

    Updated forces to use bucket approach

--- sph/interact.c
+++ sph/interact.c
@@ -31,75 +31,70 @@
  *@c*/
 
 inline
-void update_density(particle_t* pi, particle_t* pj, float h2, float C)
-{
-    float r2 = vec3_dist2(pi->x, pj->x);
-    float z  = h2-r2;
-    if (z > 0) {
-        float rho_ij = C*z*z*z;
-        pi->rho += rho_ij;
-        pj->rho += rho_ij;
-    }
+void update_density(particle_t* pi, particle_t* pj, float h2, float C) {
+	float r2 = vec3_dist2(pi->x, pj->x);
+	float z = h2 - r2;
+	if (z > 0) {
+		float rho_ij = C * z * z * z;
+		pi->rho += rho_ij;
+		pj->rho += rho_ij;
+	}
 }
 
-void compute_density(sim_state_t* s, sim_param_t* params)
-{
-    int n = s->n;
-    particle_t* p = s->part;
-    particle_t** hash = s->hash;
+void compute_density(sim_state_t* s, sim_param_t* params) {
+	int n = s->n;
+	particle_t* p = s->part;
+	particle_t** hash = s->hash;
 
-    float h  = params->h;
-    float h2 = h*h;
-    float h3 = h2*h;
-    float h9 = h3*h3*h3;
-    float C  = ( 315.0/64.0/M_PI ) * s->mass / h9;
+	float h = params->h;
+	float h2 = h * h;
+	float h3 = h2 * h;
+	float h9 = h3 * h3 * h3;
+	float C = (315.0 / 64.0 / M_PI) * s->mass / h9;
 
-    // Clear densities
-    for (int i = 0; i < n; ++i)
-        p[i].rho = 0;
+	// Clear densities
+	for (int i = 0; i < n; ++i)
+		p[i].rho = 0;
 
-    // Accumulate density info
+	// Accumulate density info
 #ifdef USE_BUCKETING
-    /* BEGIN TASK */
+	/* BEGIN TASK */
 
-    float rhoAdditive = (315.0/64.0/M_PI) * s->mass / h3;
+	float rhoAdditive = (315.0 / 64.0 / M_PI) * s->mass / h3;
 
-    // Bit flag for the particle neighbor, accelerates deduping
+	// Bit flag for the particle neighbor, accelerates deduping
 	char usedBinID[HASH_SIZE];
 	memset(usedBinID, 0, sizeof(char) * HASH_SIZE);
 
-    unsigned buckets[MAX_NBR_BINS];
-    unsigned numbins;
-    for (int i = 0; i < n; ++i) {
-    	particle_t* pi = s->part+i;
-    	pi->rho += rhoAdditive;
-    	numbins = particle_neighborhood(buckets, pi, h, usedBinID);
-    	for (int j = 0; j < numbins; ++j) {
-    		unsigned bucketid = buckets[j];
-    		for (particle_t* pj = hash[bucketid]; pj != NULL; pj = pj->next) {
-    			if (pi < pj
-    					&& abs(pi->ix - pj->ix) <= 1
-    					&& abs(pi->iy - pj->iy) <= 1
-    					&& abs(pi->iz - pj->iz) <= 1)
-    			{
-    				update_density(pi, pj, h2, C);
-    			}
-    		}
-    	}
-    }
-    /* END TASK */
+	unsigned buckets[MAX_NBR_BINS];
+	unsigned numbins;
+	for (int i = 0; i < n; ++i) {
+		particle_t* pi = s->part + i;
+		pi->rho += rhoAdditive;
+		numbins = particle_neighborhood(buckets, pi, h, usedBinID);
+		for (int j = 0; j < numbins; ++j) {
+			unsigned bucketid = buckets[j];
+			for (particle_t* pj = hash[bucketid]; pj != NULL ; pj = pj->next) {
+				if (pi < pj && abs(pi->ix - pj->ix) <= 1
+						&& abs(pi->iy - pj->iy) <= 1
+						&& abs(pi->iz - pj->iz) <= 1) {
+					update_density(pi, pj, h2, C);
+				}
+			}
+		}
+	}
+	/* END TASK */
 #else
-    for (int i = 0; i < n; ++i) {
-        particle_t* pi = s->part+i;
-        pi->rho += (315.0/64.0/M_PI) * s->mass / h3;
-        for (int j = i+1; j < n; ++j) {
-            particle_t* pj = s->part+j;
-            update_density(pi, pj, h2, C);
-        }
-    }
+	for (int i = 0; i < n; ++i) {
+		particle_t* pi = s->part+i;
+		pi->rho += (315.0/64.0/M_PI) * s->mass / h3;
+		for (int j = i+1; j < n; ++j) {
+			particle_t* pj = s->part+j;
+			update_density(pi, pj, h2, C);
+		}
+	}
 #endif
 }
-
 
 /*@T
  * \subsection{Computing forces}
@@ -117,100 +112,108 @@ void compute_density(sim_state_t* s, sim_param_t* params)
  *@c*/
 
 inline
-void update_forces(particle_t* pi, particle_t* pj, float h2,
-                   float rho0, float C0, float Cp, float Cv)
-{
-    float dx[3];
-    vec3_diff(dx, pi->x, pj->x);
-    float r2 = vec3_len2(dx);
-    if (r2 < h2) {
-        const float rhoi = pi->rho;
-        const float rhoj = pj->rho;
-        float q = sqrt(r2/h2);
-        float u = 1-q;
-        float w0 = C0 * u/rhoi/rhoj;
-        float wp = w0 * Cp * (rhoi+rhoj-2*rho0) * u/q;
-        float wv = w0 * Cv;
-        float dv[3];
-        vec3_diff(dv, pi->v, pj->v);
+void update_forces(particle_t* pi, particle_t* pj, float h2, float rho0,
+		float C0, float Cp, float Cv) {
+	float dx[3];
+	vec3_diff(dx, pi->x, pj->x);
+	float r2 = vec3_len2(dx);
+	if (r2 < h2) {
+		const float rhoi = pi->rho;
+		const float rhoj = pj->rho;
+		float q = sqrt(r2 / h2);
+		float u = 1 - q;
+		float w0 = C0 * u / rhoi / rhoj;
+		float wp = w0 * Cp * (rhoi + rhoj - 2 * rho0) * u / q;
+		float wv = w0 * Cv;
+		float dv[3];
+		vec3_diff(dv, pi->v, pj->v);
 
-        // Equal and opposite pressure forces
-        vec3_saxpy(pi->a,  wp, dx);
-        vec3_saxpy(pj->a, -wp, dx);
-        
-        // Equal and opposite viscosity forces
-        vec3_saxpy(pi->a,  wv, dv);
-        vec3_saxpy(pj->a, -wv, dv);
-    }
+		// Equal and opposite pressure forces
+		vec3_saxpy(pi->a, wp, dx);
+		vec3_saxpy(pj->a, -wp, dx);
+
+		// Equal and opposite viscosity forces
+		vec3_saxpy(pi->a, wv, dv);
+		vec3_saxpy(pj->a, -wv, dv);
+	}
 }
 
-void compute_accel(sim_state_t* state, sim_param_t* params)
-{
-    // Unpack basic parameters
-    const float h    = params->h;
-    const float rho0 = params->rho0;
-    const float k    = params->k;
-    const float mu   = params->mu;
-    const float g    = params->g;
-    const float mass = state->mass;
-    const float h2   = h*h;
+void compute_accel(sim_state_t* state, sim_param_t* params) {
+	// Unpack basic parameters
+	const float h = params->h;
+	const float rho0 = params->rho0;
+	const float k = params->k;
+	const float mu = params->mu;
+	const float g = params->g;
+	const float mass = state->mass;
+	const float h2 = h * h;
 
-    // Unpack system state
-    particle_t* p = state->part;
-    particle_t** hash = state->hash;
-    int n = state->n;
+	// Unpack system state
+	particle_t* p = state->part;
+	particle_t** hash = state->hash;
+	int n = state->n;
 
-    // Rehash the particles
-    hash_particles(state, h);
+	// Rehash the particles
+	hash_particles(state, h);
 
-    // Compute density and color
-    compute_density(state, params);
+	// Compute density and color
+	compute_density(state, params);
 
-    // Start with gravity and surface forces
-    for (int i = 0; i < n; ++i)
-        vec3_set(p[i].a,  0, -g, 0);
+	// Start with gravity and surface forces
+	for (int i = 0; i < n; ++i)
+		vec3_set(p[i].a, 0, -g, 0);
 
-    // Constants for interaction term
-    float C0 = 45 * mass / M_PI / ( (h2)*(h2)*h );
-    float Cp = k/2;
-    float Cv = -mu;
+	// Constants for interaction term
+	float C0 = 45 * mass / M_PI / ((h2) * (h2) * h);
+	float Cp = k / 2;
+	float Cv = -mu;
 
-    // Accumulate forces
+	// Accumulate forces
 #ifdef USE_BUCKETING
-    /* BEGIN TASK */
+	/* BEGIN TASK */
 
-    // Bit flag for the particle neighbor, accelerates deduping
+	// Bit flag for the particle neighbor, accelerates deduping
 	char usedBinID[HASH_SIZE];
 	memset(usedBinID, 0, sizeof(char) * HASH_SIZE);
 
-    unsigned buckets[MAX_NBR_BINS];
-    unsigned numbins;
-    for (int i = 0; i < n; ++i) {
-    	particle_t* pi = p+i;
-    	numbins = particle_neighborhood(buckets, pi, h, usedBinID);
-    	for (int j = 0; j < numbins; ++j) {
-    		unsigned bucketid = buckets[j];
-    		for (particle_t* pj = hash[bucketid]; pj != NULL; pj = pj->next) {
-    			if (pi < pj
-    					&& abs(pi->ix - pj->ix) <= 1
-    					&& abs(pi->iy - pj->iy) <= 1
-    					&& abs(pi->iz - pj->iz) <= 1)
-    			{
-    				update_forces(pi, pj, h2, rho0, C0, Cp, Cv);
-    			}
-    		}
-    	}
-    }
+	unsigned buckets[MAX_NBR_BINS];
+	unsigned numbins;
 
-    /* END TASK */
+	// Iterate over each bucket, and within each bucket each particle
+	for (int iter_bucket = 0; iter_bucket < HASH_SIZE; ++iter_bucket)
+	{
+		for (particle_t* pi = hash[iter_bucket]; pi != NULL ; pi = pi->next)
+		{
+			// Compute equal and opposite forces for the particle,
+			// first get neighbors
+			numbins = particle_neighborhood(buckets, pi, h, usedBinID);
+			for (int j = 0; j < numbins; ++j)
+			{
+				// Get the neighbor particles
+				unsigned bucketid = buckets[j];
+				for (particle_t* pj = hash[bucketid]; pj != NULL ; pj =
+						pj->next) {
+					// Compute forces only if appropriate
+					if (pi < pj && abs(pi->ix - pj->ix) <= 1
+							&& abs(pi->iy - pj->iy) <= 1
+							&& abs(pi->iz - pj->iz) <= 1) {
+						update_forces(pi, pj, h2, rho0, C0, Cp, Cv);
+					}
+				}
+			}
+		}
+	}
+}
+
+/* END TASK */
 #else
-    for (int i = 0; i < n; ++i) {
-        particle_t* pi = p+i;
-        for (int j = i+1; j < n; ++j) {
-            particle_t* pj = p+j;
-            update_forces(pi, pj, h2, rho0, C0, Cp, Cv);
-        }
-    }
+for (int i = 0; i < n; ++i) {
+	particle_t* pi = p+i;
+	for (int j = i+1; j < n; ++j) {
+		particle_t* pj = p+j;
+		update_forces(pi, pj, h2, rho0, C0, Cp, Cv);
+	}
+}
 #endif
 }
 
